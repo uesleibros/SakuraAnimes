bd537415eee5eedaae28203b68d6f038da293003
Ueslei Paim <uesleibros@gmail.com>
2024-08-16 01:19:25 -0300

    Update page.js

--- src/app/testar-player/page.js
+++ src/app/testar-player/page.js
@@ -1,20 +1,17 @@
 "use client";
 
 import Script from "next/script";
-import Head from "next/head";
 export default function TestarPlayer() {
   
   return (
     <main className="min-h-screen">
-      <Head>
-        <Script 
-          src="/lib/playerjs.js"
-          strategy="beforeInteractive"
-          onLoad={() => {
-            var player = new Playerjs({ id:"player", file:"https://raw.githubusercontent.com/nicaksks/EnmaAi/main/assets/one-piece-movie-13/001.m3u8", title: "One Piece Gold: O Filme" });
-          }}
-        />
-      </Head>
+      <Script 
+        src="/lib/playerjs.js"
+        strategy="beforeInteractive"
+        onLoad={() => {
+          var player = new Playerjs({ id:"player", file:"https://raw.githubusercontent.com/nicaksks/EnmaAi/main/assets/one-piece-movie-13/001.m3u8", title: "One Piece Gold: O Filme" });
+        }}
+      />
       <header className="px-10 py-5 border-b shadow-sm">
         <h2 className="font-bold text-2xl text-pink-400">Sakura Animes — Testar Player</h2>
       </header>
