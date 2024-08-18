ea3a0dea6667e3f4e00573d0f9f3861ef09913ad
Ueslei Paim <uesleibros@gmail.com>
2024-08-18 03:18:53 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
   ],
   safelist: [
     {
-      pattern: /bg-(blue|green|purple|pink|red|amber|yellow)-(400|500|600|800|900)/,
+      pattern: /bg-(blue|green|orange|purple|pink|red|amber|yellow)-(400|500|600|800|900)/,
     },
     {
-      pattern: /text-(blue|green|purple|pink|red|amber|yellow)-(400|500|600|800|900)/,
+      pattern: /text-(blue|green|orange|purple|pink|red|amber|yellow)-(400|500|600|800|900)/,
     },
   ],
   theme: {
