d58e7f1bf49f0afa900139f775f7d8ab1819b5a8
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-08-18 11:42:02 -0300

    melhorado qualidade de imagens

--- src/app/assistir/anime/[slug]/page.jsx
+++ src/app/assistir/anime/[slug]/page.jsx
@@ -182,7 +182,7 @@ export default function VerAnime({params}) {
 								</div>
 							</div>
 						) : (
-							<Spinner className="mt-10" />
+							<Spinner className={`mt-10 ${anime.type === "movie" && "invisible"}`} />
 						)}
 					</div>
 				) : (
