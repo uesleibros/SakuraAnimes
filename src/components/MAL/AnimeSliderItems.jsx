b1bc78ecd3c57ce8ecaf517d77970b726f625b6c
Ueslei Paim <uesleibros@gmail.com>
2024-08-24 01:09:22 -0300

    Update AnimeSliderItems.jsx

--- src/components/MAL/AnimeSliderItems.jsx
+++ src/components/MAL/AnimeSliderItems.jsx
@@ -27,7 +27,7 @@ export default function AnimeSliderItems({ items }) {
 	};
 
 	return (
-		<Slider {...settings} className="w-full h-[334px]">
+		<Slider {...settings} className="w-full h-[340px]">
 			{items.map((item, index) => (
 			  <Link key={index} href={`/assistir/anime/${item.mal_id}`} className="!w-[200px] h-[max-content] relative group">
 			    <div className="w-full h-[323px] relative">
