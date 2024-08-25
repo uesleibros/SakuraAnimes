c39b57d4ac6959cc9d32b609b3ab99441600ebf4
Ueslei Paim <uesleibros@gmail.com>
2024-08-24 23:55:27 -0300

    Update route.js

--- src/app/api/buscar/animes/anroll/route.js
+++ src/app/api/buscar/animes/anroll/route.js
@@ -7,7 +7,7 @@ async function pegarInformacoesDetalhadasAnime(data) {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
 			"Referer": "https://www.anroll.net/"
 		},
-		cache: "no-store"
+		next: { revalidate: 300 }
 	});
 
 	const extraData = extractData(await res.text());
