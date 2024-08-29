9165e1da132346ffba523a7736a539ce6a8d2e9a
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-08-29 17:18:38 -0300

    atualizado novo servidor da anroll

--- src/app/api/buscar/animes/anroll/episodios/navegar/route.js
+++ src/app/api/buscar/animes/anroll/episodios/navegar/route.js
@@ -4,7 +4,7 @@ export async function GET(request) {
 	if (!id)
 		return Response.json({ error: "missing ep_id." }, { status: 404 });
 
-	const res = await fetch(`https://www.anroll.net/_next/data/DNs-ereP64IysZZGkZXqY/e/${id}.json?episode=${id}`, {
+	const res = await fetch(`https://www.anroll.net/_next/data/nxb43Ok4qvT2cAlD0OLS1/e/${id}.json?episode=${id}`, {
 		headers: {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
 			"Referer": "https://www.anroll.net/"
