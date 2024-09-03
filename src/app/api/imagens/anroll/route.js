032f1e998f3cc326e73cf0db9e4f7d237993d1b9
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-09-03 13:39:45 -0300

    testando cloudflare do anroll.

--- src/app/api/imagens/anroll/route.js
+++ src/app/api/imagens/anroll/route.js
@@ -16,10 +16,10 @@ export async function GET(request) {
     const res = await fetch(`${url}&__cf_chl_rt_tk=mkrdVHTBzhTDAR0lA4vkagRNKH2MOHkFhCo0068kn_4-1725379337-0.0.1.1-7316`, {
       agent,
       headers: {
-        "Authority": "vidroll.cloud",
-        "Referer": url,
-        "Host": "www.anroll.net",
-        "Origin": `https://${new URL(url).host}`,
+        "Authority": "c8.vidroll.cloud",
+        "Referer": "https://www.anroll.net/",
+        "Host": "https://www.anroll.net",
+        "Origin": "https://www.anroll.net",
         "Cookie": "cf_chl_rc_i=14; cf_clearance=sn64to8rx_RtsuIE8gYRmHaSRVvdPj5IBEwDRVxK1qM-1725381127-1.2.1.1-fLBtKZNM003d3L1H6M0pqtRDTW77mRi5c8NfOYAs3sk.RMWJwqu2P1Y4GAKQ1Tbb38cXLFHQRBEdsrvTlKHMwO_4TddJt0pkvwrhSWH44fe4W2MtuV4Wh5rVzHDGM75QYgqHKI0gz558XAE69KU5b6JhGHz.zdaO9xUp7xTsh5J22iBerMXIswP0EBvHtrqqUzoek1XEEv9Btee2k2y3jWR9e7Bufx.yeBZUEt1AtCZ5igE_R60wLZew7HBAtf8fqLWBbWQWAl83fuHlw.zogKVqAhcULdJ9IQgF3BK1KjB5UvhDEwrJYPq_krzMMrcVtlPWVLRCC1Z7DWyRJyub7Ulud5QZWoUS8vahaWwUdpuwhOPLQqwJL9DoaM3Epnwhl5IlMNIe1HkOB9C9ElWOa4EiBVcUYSzqUu17SQ2EIjbDaEQ7VHYutDa1hWW9S6ji",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
       },
