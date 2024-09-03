75c38a434ce98937da995c8d5554965de434c244
Ueslei Paim <uesleibros@gmail.com>
2024-09-02 23:18:13 -0300

    Update route.js

--- src/app/api/imagens/anroll/route.js
+++ src/app/api/imagens/anroll/route.js
@@ -13,11 +13,12 @@ export async function GET(request) {
       method: "HEAD",
       headers: {
         "Authority": new URL(url).host,
-        "Host": "anroll.net",
-        "Content-Length": 55,
-        "Origin": "https://www.anroll.net",
-        "Referer": url,
-        "Path": new URL(url).pathname,
+        "Host": new URL(url).host,
+        "Accept": "image/webp",
+        "Connection": "keep-alive",
+        "Mode": "cors",
+        "Origin": `https://${new URL(url).host}`,
+        "Referer": "https://www.anroll.net/",
         "Cf-Ray": "8bc942b6981b5210-GRU",
         "Nel": '{"success_fraction":0,"report_to":"cf-nel","max_age":604800}',
         "Report-To": '{"endpoints":[{"url":"https:\/\/a.nel.cloudflare.com\/report\/v4?s=5cULeDVZhT2XGTomTHZHBNgkCWrDOMuKCY1imqjyaln4eueUt%2F3W0VHTYV1s0iZ1k%2B5LO%2B5olLC2L5Iw6CJeWk8Z8gMU85aMIummLJ9WoV6QWYxn45cJuguTZD3tF8sZ8pLcjz8e1j6afRMPppvv"}],"group":"cf-nel","max_age":604800}',
@@ -29,7 +30,7 @@ export async function GET(request) {
         "X-Remote-Addr": "172.67.177.146",
         "Cluster-Client-IP": "172.67.177.146",
         "X-HTTP-Method-Override": "HEAD",
-        "User-Agent": "Mozilla/5.0 (X11; U; Linux i686; en-GB; rv:1.7.6) Gecko/20050405 Epiphany/1.6.1 (Ubuntu) (Ubuntu package 1.0.2)",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
       }
     });
     console.log(testt.headers);
