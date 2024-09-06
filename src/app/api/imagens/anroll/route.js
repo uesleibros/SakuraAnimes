36d5ea76f8b2a60e295daed9e13b2d364ade7383
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-09-06 16:59:06 -0300

    arrumado possivelmente coiso do cloudflare

--- src/app/api/imagens/anroll/route.js
+++ src/app/api/imagens/anroll/route.js
@@ -1,3 +1,5 @@
+import axios from "axios":
+
 export async function GET(request) {
   const query = request.nextUrl.searchParams.get("q");
 
@@ -11,39 +13,28 @@ export async function GET(request) {
     const url = decodeURIComponent(query);
     const apikey = "ed59bacd6da711d678cf8baa6966aba6350d364a";
 
-    const res = await fetch(`https://api.zenrows.com/v1?apikey=${apikey}&original_status=true&custom_headers=true&url=${url}`, {
+    const res = await axios({
+      url: "https://api.zenrows.com/v1/",
+      method: "GET",
       headers: {
         "Referer": "https://www.anroll.net/",
-        "Sec-Ch-Ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-        "Sec-Ch-Ua-Arch": '"x86"',
-        "Sec-Ch-Ua-Bitness": '"64"',
-        "Sec-Ch-Ua-Full-Version": '"128.0.6613.117"',
-        "Sec-Ch-Ua-Full-Version-List": '"Chromium";v="128.0.6613.117", "Not;A=Brand";v="24.0.0.0", "Google Chrome";v="128.0.6613.117"',
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Model": '',
-        "Sec-Ch-Ua-Platform": '"Windows"',
-        "Sec-Ch-Ua-Platform-Version": '"10.0.0"',
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": "\"Windows\"",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-User": "?1",
-        "Service-Worker-Navigation-Preload": "true",
-        "Upgrade-Insecure-Requests": '1',
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
       },
-      cache: "no-store",
+      params: {
+        "url": url,
+        "apikey": apikey,
+        "custom_headers": "true",
+      },
+      responseType: "arrayBuffer"
     });
 
-    if (!res.ok) {
+    if (res.status !== 200) {
       return new Response(
         JSON.stringify({ error: "Failed to fetch the image." }),
         { status: res.status, headers: { "Content-Type": "application/json" } }
       );
     }
 
-    const imageBuffer = await res.arrayBuffer();
+    const imageBuffer = res.data;
     const contentLength = imageBuffer.byteLength;
 
     return new Response(imageBuffer, {
